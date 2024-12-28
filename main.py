5cd6352b02341f19d2096b5cef62b8c3322eaf8d
Ayman El Amrani <ayman.elamrani99@gmail.com>
2024-12-28 19:48:19 +0100

    rename columns

--- main.py
+++ main.py
@@ -16,8 +16,30 @@ with open('drone_coords.SRT', 'r') as file:
     drone_coords = [extract_coords(r) for r in raw_coords]
 
 with open('drone_coords.csv', 'w', newline='') as file:
-    columns = [['frame_num', 'drone_latitude', 'drone_longitude', 'displacement_from_origin', 'displacement_from_previous', 'is_reference']]
+    columns = [['frame_num', 'drone_latitude', 'drone_longitude', 'displacement_origin', 'displacement_previous', 'is_reference']]
     data = [[frame + 1, *coords, None, None, frame + 1 in reference_frames] for frame, coords in enumerate(drone_coords)]
     writer = csv.writer(file)
     writer.writerows(columns + data)
+
+# def get_displacement_vectors(input_coords, start, end):
+#     displacements = []
+#     for idx in range(len(reference_coords)):
+#         if idx == 0:
+#             displacements.append([0, 0])
+#         else:
+#             long_curr = reference_coords[idx][1]
+#             long_prev = reference_coords[idx - 1][1]
+#             lat_curr = reference_coords[idx][2]
+#             lat_prev = reference_coords[idx - 1][2]
+#             displacement = ((long_curr - long_prev), (lat_curr - lat_prev))
+#             displacements.append(displacement)
+
+
+# print('\n'.join([str(u) for u in reference_coords]))
+# print('\n'.join([str(d) for d in displacements]))
+
+# data = [['frame_num', 'drone_latitude', 'drone_longitude', 'displacement_from_origin', 'displacement_from_previous']]
+# with open('drone_coords.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerows(data)
 
